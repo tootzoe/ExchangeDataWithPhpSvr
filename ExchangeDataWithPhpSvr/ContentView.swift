edf6bf23421f32dfefa484b4e7696d3528e56a31
toot_hzf <toot@tootzoe.com>
2024-04-04 17:56:05 +0700

    Initial Commit

---
+++ ExchangeDataWithPhpSvr/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  ExchangeDataWithPhpSvr
+//
+//  Created by thor on 4/4/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExchangeDataWithPhpSvr/ExchangeDataWithPhpSvrApp.swift
@@ -0,0 +1,23 @@
+//
+//  ExchangeDataWithPhpSvrApp.swift
+//  ExchangeDataWithPhpSvr
+//
+//  Created by thor on 4/4/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+import SwiftUI
+
+@main
+struct ExchangeDataWithPhpSvrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
