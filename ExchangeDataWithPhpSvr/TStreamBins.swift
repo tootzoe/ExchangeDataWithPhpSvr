389aecbd186ee54be277597ad196bab5d73d5664
toot_hzf <toot@tootzoe.com>
2024-04-04 23:46:01 +0700

    Initiali commit....

---
+++ ExchangeDataWithPhpSvr/TStreamBins.swift
@@ -0,0 +1,14 @@
+//
+//  TStreamBins.swift
+//  ExchangeDataWithPhpSvr
+//
+//  Created by thor on 4/4/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+import Foundation
